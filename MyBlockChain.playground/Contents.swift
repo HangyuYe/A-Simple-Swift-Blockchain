ad46c7ff6e1f1b33934efe1f7fef026459634d71
Hangyu Ye <jerry.hangyu@gmail.com>
2018-08-07 05:39:16 +0100

    Main Files Added

---
+++ MyBlockChain.playground/Contents.swift
@@ -0,0 +1,11 @@
+//Copyright SoftChain Foundation Ltd. 2018, All Rights Reserved
+//My First BlockChain using Swift4 in Playground
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct Block {
+    public private(set) var previousBlockHashValuse: Int
+    public private(set) var transactions: [String]
+
+}
