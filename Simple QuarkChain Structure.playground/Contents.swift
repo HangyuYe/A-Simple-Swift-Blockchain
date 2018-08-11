5aeb559be00e7c9bc8c22d64738c08b72b9d304b
Hangyu Ye <jerry.hangyu@gmail.com>
2018-08-11 17:13:59 +0800

    added files for strct of QuarkChain Network demo

---
+++ Simple QuarkChain Structure.playground/Contents.swift
@@ -0,0 +1,12 @@
+//Copyright (c) SoftChain Foundation Ltd. 2018, All Rights Reserved
+//The Project and code is not remain for QuarkChain Network.
+
+import Foundation
+
+var str = "Hello, I am demo structure of QuarkChain Network"
+
+print(str)
+
+
+
+
